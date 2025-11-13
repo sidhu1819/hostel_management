c555579d89dc479636ef543f7980fe98abe0a878
sidhu1819 <832SIDDHARTHA@GMAIL.COM>
2025-11-13 17:11:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,10 @@ mongoose
     process.exit(1);
   });
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/rooms", roomRoutes);
