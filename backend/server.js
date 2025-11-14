dd66dceac49248a327a1bd0f02c2c20537469322
sidhu1819 <832SIDDHARTHA@GMAIL.COM>
2025-11-14 10:16:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:3001"],
+  origin: ["http://localhost:3000", "http://localhost:3001","https://hostel-management-system-rnox.onrender.com/"],
   credentials: true,
 }));
 
